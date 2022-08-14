2d792faf6682c080889109592b0c1c75999fbba4
natlmartin <106486643+natlmartin@users.noreply.github.com>
2022-08-14 17:39:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const state = {
     numCells: (600/40) * (600/40), 
     cells: []
-    shipPosition: 217
+    shipPosition: 217,
     alienPositions: [
     3, 4, 5, 6, 7, 8, 9, 10,11,
     18,19,20,21,22,23,24,25,26,
